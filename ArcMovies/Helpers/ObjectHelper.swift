ebeabde92562fb7762fa9e0f97a1031a21161292
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-12 19:17:19 -0300

    Create Object Helper to help properties initialization of an object

---
+++ ArcMovies/Helpers/ObjectHelper.swift
@@ -0,0 +1,19 @@
+//
+//  ObjectHelper.swift
+//  ArcMovies
+//
+//  Created by Jaime Costa Marques on 12/05/18.
+//  Copyright © 2018 Jaime Costa Marques. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Establishes a object to be used for the execution of statements,
+ potentially reducing the amount of code that needs to be written.
+ */
+@discardableResult
+internal func With<ObjectTarget>(_ target: inout ObjectTarget, block: (_ target: inout ObjectTarget) -> Void) -> ObjectTarget {
+    block(&target)
+    return target
+}
