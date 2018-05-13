4ad08c1e215a869486472a713b5acc19a8a3dbc6
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-13 02:27:34 -0300

    - Adjust poster cell positioning - Add targetContentOffset for centralisation scrolling

--- ArcMovies/MoviesScene/CollectionView/Layout/MoviesFlowLayout.swift
+++ ArcMovies/MoviesScene/CollectionView/Layout/MoviesFlowLayout.swift
@@ -13,6 +13,9 @@ class MoviesFlowLayout: UICollectionViewFlowLayout {
     fileprivate var lastCollectionViewSize: CGSize = CGSize.zero
 
     private let centerOffset: CGFloat = 0
+    private let scaleOffset: CGFloat = 200
+    private let minScaleFactor: CGFloat = 0.8
+    private let minAlphaFactor: CGFloat = 0.5
 
     init(itemSize: CGSize) {
         super.init()
@@ -31,7 +34,7 @@ extension MoviesFlowLayout {
     
     fileprivate func commonInit(_ itemSize: CGSize) {
         
-        minimumLineSpacing = 25
+        minimumLineSpacing = -5
         scrollDirection = .horizontal
         self.itemSize = itemSize
     }
@@ -47,10 +50,9 @@ extension  MoviesFlowLayout {
             return
         }
 
-        let centerInset = collectionView.bounds.size.width / 2 - itemSize.width / 2
-        let centerDisplacement = centerInset / 2
-        collectionView.contentInset = UIEdgeInsetsMake(0, centerInset - centerDisplacement, 0, centerInset)
-        collectionView.contentOffset = CGPoint(x: -centerInset, y: 0)
+        let inset = collectionView.bounds.size.width / 2 - itemSize.width / 2
+        collectionView.contentInset = UIEdgeInsetsMake(0, inset, 0, inset)
+        collectionView.contentOffset = CGPoint(x: -inset, y: 0)
     }
 
 }
@@ -67,38 +69,76 @@ extension MoviesFlowLayout {
             lastCollectionViewSize = collectionView.bounds.size
         }
     }
-
+    
+    override func shouldInvalidateLayout(forBoundsChange _: CGRect) -> Bool {
+        return true
+    }
+    
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         guard let collectionView = self.collectionView,
-              let superAttributes = super.layoutAttributesForElements(in: rect) else {
+              let elementsArray = super.layoutAttributesForElements(in: rect) else {
             return super.layoutAttributesForElements(in: rect)
         }
-
-        let scaleOffset: CGFloat = 200
-        let minScaleFactor: CGFloat = 0.7
-        let minAlphaFactor: CGFloat = 0.3
+        
         let size = collectionView.bounds.size
         let contentOffset = collectionView.contentOffset
 
         let visibleRect = CGRect(x: contentOffset.x, y: contentOffset.y, width: size.width, height: size.height)
         let visibleCenterX = visibleRect.midX
 
-        guard case let elementsAttributesArray as [UICollectionViewLayoutAttributes] = NSArray(array: superAttributes, copyItems: true) else {
-            return nil
-        }
-
-        elementsAttributesArray.forEach {
+        elementsArray.forEach {
             let distanceFromCenter = visibleCenterX - $0.center.x
-            let absDistanceFromCenter = min(abs(distanceFromCenter), minScaleFactor)
-            let scale = absDistanceFromCenter * minScaleFactor / scaleOffset + 1
+            let absDistanceFromCenter = min(abs(distanceFromCenter), scaleOffset)
+            let scale = absDistanceFromCenter * (self.minScaleFactor - 1) / self.scaleOffset + 1
             $0.transform3D = CATransform3DScale(CATransform3DIdentity, scale, scale, 1)
-            //$0.transform3D = CATransform3DMakeScale(1 + (minScaleFactor - 1) * absDistanceFromCenter, 1 + (minScaleFactor - 1) * absDistanceFromCenter, 1)
-
-
-            let alpha = absDistanceFromCenter * (minAlphaFactor - 1) / scaleOffset + 1
+            
+            let alpha = absDistanceFromCenter * (self.minAlphaFactor - 1) / self.scaleOffset + 1
             $0.alpha = alpha
         }
 
-        return elementsAttributesArray
+        return elementsArray
+    }
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        guard let collectionView = self.collectionView else {
+            return proposedContentOffset
+        }
+        
+        let proposedRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.width, height: collectionView.bounds.height)
+        guard let layoutAttributes = self.layoutAttributesForElements(in: proposedRect) else {
+            return proposedContentOffset
+        }
+        
+        var candidateAttributes: UICollectionViewLayoutAttributes?
+        let proposedContentOffsetCenterX = proposedContentOffset.x + collectionView.bounds.width / 2
+        
+        for attributes in layoutAttributes {
+            if attributes.representedElementCategory != .cell {
+                continue
+            }
+            
+            if candidateAttributes == nil {
+                candidateAttributes = attributes
+                continue
+            }
+            
+            if fabs(attributes.center.x - proposedContentOffsetCenterX) < fabs(candidateAttributes!.center.x - proposedContentOffsetCenterX) {
+                candidateAttributes = attributes
+            }
+        }
+        
+        guard let aCandidateAttributes = candidateAttributes else {
+            return proposedContentOffset
+        }
+        
+        var newOffsetX = aCandidateAttributes.center.x - collectionView.bounds.size.width / 2
+        let offset = newOffsetX - collectionView.contentOffset.x
+        
+        if (velocity.x < 0 && offset > 0) || (velocity.x > 0 && offset < 0) {
+            let pageWidth = itemSize.width + minimumLineSpacing
+            newOffsetX += velocity.x > 0 ? pageWidth : -pageWidth
+        }
+        
+        return CGPoint(x: newOffsetX, y: proposedContentOffset.y)
     }
 }
