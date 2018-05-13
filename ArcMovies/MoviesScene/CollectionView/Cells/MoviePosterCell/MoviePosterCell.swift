a1f02e5df443ce11095dafb577d72d1f1c71005d
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-13 19:11:24 -0300

    Implement dominant color to handler titles, border and shadow color - Create protrocol Colorable - Add color helper to darken or lighten colors - Add UIImageColors lib

--- ArcMovies/MoviesScene/CollectionView/Cells/MoviePosterCell/MoviePosterCell.swift
+++ ArcMovies/MoviesScene/CollectionView/Cells/MoviePosterCell/MoviePosterCell.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+// _Colorable_ is a protocol to handler dominant colors
+protocol Colorable {
+    func dominantColor( _ primary: UIColor )
+}
+
 // MARK: -  MoviePosterCell
 
 /// _MoviePosterCell_ is the collection view cell responsible to display our Movie poster
@@ -19,6 +24,7 @@ final class MoviePosterCell: UICollectionViewCell {
     
     private let cornerRadius: CGFloat = 10
     private var shadowView: UIView? = nil
+    var colorable: Colorable!
     
     var viewModel: MoviesViewModel? {
         didSet {
@@ -41,6 +47,8 @@ final class MoviePosterCell: UICollectionViewCell {
         super.prepareForReuse()
         Poster.alpha = 0
         Poster.image = nil
+        colorable.dominantColor(UIColor.black)
+        shadowView?.alpha = 0
         shadowView?.layer.shadowPath = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height), cornerRadius: PosterContainer.layer.cornerRadius).cgPath
     }
     
@@ -107,8 +115,8 @@ extension MoviePosterCell {
             //Show Poster animated
             self?.showPoster()
             
-            // Show or Create shadow
-            self?.configureShadow()
+            // Configure colors
+            self?.defineColors(with: self?.Poster.image)
         }
     }
     
@@ -122,16 +130,12 @@ extension MoviePosterCell {
     }
     
     private func showShadow(_ shadow : UIView?) {
-        guard shadowView == nil else { return }
         DispatchQueue.main.async {
             UIView.animate(withDuration: 0.2) { [weak self] in
                 shadow?.alpha = 1
                 self?.layoutIfNeeded()
             }
         }
-        
-        // Store shadow
-        shadowView = shadow
     }
 
     private func configureViews() {
@@ -146,7 +150,11 @@ extension MoviePosterCell {
     }
     
     private func configureShadow() {
-        guard self.shadowView == nil else { return }
+        guard self.shadowView == nil else {
+            // Show shadow
+            showShadow(self.shadowView)
+            return
+        }
         var shadowView = UIView(frame: .zero)
 
         // Set poster shadow
@@ -154,14 +162,40 @@ extension MoviePosterCell {
             $0.alpha = 0
             $0.backgroundColor = UIColor(white: 0, alpha: 0)
             $0.layer.masksToBounds = false
-            $0.layer.shadowColor = UIColor.black.cgColor
-            $0.layer.shadowRadius = 10
-            $0.layer.shadowOpacity = 0.3
-            $0.layer.shadowOffset = CGSize(width: 0, height: 5)
+            $0.layer.shadowColor = UIColor(white: 0, alpha: 0).cgColor
+            $0.layer.shadowRadius = 25
+            $0.layer.shadowOpacity = 0.5
+            $0.layer.shadowOffset = CGSize(width: 0, height: 8)
             $0.layer.shouldRasterize = true
             $0.layer.shadowPath = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height), cornerRadius: PosterContainer.layer.cornerRadius).cgPath
         }
         addShowViewWithConstraint(shadowView)
+    }
+    
+    /// Define views colors
+    private func defineColors(with image: UIImage?) {
+        guard let image = image else { return }
+        DispatchQueue.main.async {
+            image.getColors { [weak self] colors in
+                self?.setShadowColor(with: colors.background.cgColor)
+
+                // Picks the primary color and darkens it
+                guard let darkColor = colors.secondary.darker(by: 40) else { return }
+                self?.colorable.dominantColor(darkColor)
+                
+                // Add border to avoid white poster background
+                self?.PosterContainer.layer.borderColor = darkColor.withAlphaComponent(0.2).cgColor
+                self?.PosterContainer.layer.borderWidth = 1
+            }
+        }
+    }
+    
+    /// Set shadow color dominant
+    private func setShadowColor(with color: CGColor) {
+        self.shadowView?.layer.shadowColor = color
+        
+        // Show or Create shadow
+        configureShadow()
     }
 }
 
@@ -179,8 +213,8 @@ extension MoviePosterCell {
         shadow.bottomAnchor.constraint(equalTo: PosterContainer.bottomAnchor).isActive = true
         shadow.trailingAnchor.constraint(equalTo: PosterContainer.trailingAnchor).isActive = true
         shadow.leadingAnchor.constraint(equalTo: PosterContainer.leadingAnchor).isActive = true
-        
-        // Show shadow
-        showShadow(shadow)
+
+        // Store shadow
+        shadowView = shadow
     }
 }
