df448b83cd466aa6cb4cf15cf41c3f19317ecb85
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-13 03:38:45 -0300

    Add localised error messages

--- ArcMovies/Common/Strings.swift
+++ ArcMovies/Common/Strings.swift
@@ -15,4 +15,11 @@ struct Strings {
         
         static let title = NSLocalizedString("Upcoming movies", comment: "List of upcoming movies in theatres")
     }
+    
+    struct Error {
+        
+        static let genericTitle = NSLocalizedString("Sorry", comment: "Generic error title")
+        static let genericMessage = NSLocalizedString("Something went wrong.", comment: "Generic error message")
+        static let okButtonTitle = NSLocalizedString("Ok", comment: "Alert button title")
+    }
 }
