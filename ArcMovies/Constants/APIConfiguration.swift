24723ec7cf50557feaf8b504fd1c922060979e75
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-13 04:08:35 -0300

    Update APIConfigurator - Add endpoints - Add image MovieDB image URL

--- ArcMovies/Constants/APIConfiguration.swift
+++ ArcMovies/Constants/APIConfiguration.swift
@@ -6,14 +6,40 @@
 //  Copyright © 2018 Jaime Costa Marques. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct MovieDBAPI {
     
     static let baseUrl: BaseURLType = .production
+    static let imageUrl: BaseURLType = .imageProduction
     static let apiKey = "060fe300cc5a08ba0515ed2dd96af98f"
 }
 
 public enum BaseURLType : String {
-    case production = "https://api.themoviedb.org/3"
+    case production = "https://api.themoviedb.org/3/"
+    case imageProduction = "https://image.tmdb.org/t/p/w300"
+}
+
+// MARK: - MovieDBAPIEndpoint
+
+/// _MovieDBAPIEndpoint_ is an enumeration of all Last FM types of API requests
+///
+/// - getUpcomingMovies: The get upcoming movies request
+enum MovieDBAPIEndpoint {
+    
+    case getUpcomingMovies(Int)
+}
+
+
+// MARK: - URLConvertible
+
+extension MovieDBAPIEndpoint: URLConvertible {
+    
+    func url() -> URL? {
+        switch self {
+        case .getUpcomingMovies(let page):
+            let endpoint = "movie/upcoming"
+            return URL(string: "\(MovieDBAPI.baseUrl.rawValue)\(endpoint)?api_key=\(MovieDBAPI.apiKey)&page=\(page)")
+        }
+    }
 }
