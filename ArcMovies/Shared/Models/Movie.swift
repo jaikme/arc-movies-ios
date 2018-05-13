2f8ed10cf93dd4c95bba2800f2d4765d01fec9e2
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-13 20:31:57 -0300

    Remove Movie model unused properties

--- ArcMovies/Shared/Models/Movie.swift
+++ ArcMovies/Shared/Models/Movie.swift
@@ -16,9 +16,7 @@ struct Movie {
     let id: Int
     let title: String
     let posterPath: String
-    let overview: String
     let voteAverage: Double
-    let releaseDate: String
     let posterURL: URL?
 }
 
@@ -29,9 +27,7 @@ extension Movie : Parsable {
         static let idKey = "id"
         static let titleKey = "title"
         static let posterPathKey = "poster_path"
-        static let overviewKey = "overview"
         static let voteAverage = "vote_average"
-        static let releaseDateKey = "release_date"
     }
     
     static func fromJSON(json: [String: Any]) -> Movie? {
@@ -40,14 +36,12 @@ extension Movie : Parsable {
             let id = json[Constants.idKey] as? Int,
             let title = json[Constants.titleKey] as? String,
             let posterPath = json[Constants.posterPathKey] as? String,
-            let overview = json[Constants.overviewKey] as? String,
             let voteAverage = json[Constants.voteAverage] as? Double,
-            let releaseDate = json[Constants.releaseDateKey] as? String,
             let posterUrl = URL(string: "\(MovieDBAPI.imageUrl.rawValue)\(posterPath)") else {
             return nil
         }
         
-        return Movie(id: id, title: title, posterPath: posterPath, overview: overview, voteAverage: voteAverage, releaseDate: releaseDate, posterURL: posterUrl)
+        return Movie(id: id, title: title, posterPath: posterPath, voteAverage: voteAverage, posterURL: posterUrl)
     }
 }
 
